c973551c2ac0854cfa1650ff374eaee9e2fdb450
Aleksandr Tcikin <sunnyday.development@gmail.com>
2018-11-21 21:47:53 +0300

    Restore session when extension is shown.

--- App/SafariExtension/Sources/Classes/RichHarvestSafariViewController.swift
+++ App/SafariExtension/Sources/Classes/RichHarvestSafariViewController.swift
@@ -4,12 +4,50 @@
 
 import Foundation
 
+import RxSwift
+
 import RichHarvest_Core_Core
+import RichHarvest_Domain_Auth_Api
 
 public class SafariExtensionRootViewController: NSTabViewController {
 
+    private var viewModel: SafariExtensionRootViewModel!
+
     deinit {
         Log.debug("Deinited")
+    }
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension SafariExtensionRootViewController {
+
+    func inject(viewModel: SafariExtensionRootViewModel) {
+        self.viewModel = viewModel
+    }
+
+}
+
+class SafariExtensionRootViewModel {
+
+    private let authRepository: AuthRepository
+    private let schedulers: Schedulers
+
+    private let dispose = DisposeBag()
+
+    init(authRepository: AuthRepository, schedulers: Schedulers) {
+        self.authRepository = authRepository
+        self.schedulers = schedulers
+    }
+
+    func viewDidLoad() {
+        authRepository.restoreSession()
+            .observeOn(schedulers.ui)
+            .subscribe(onSuccess: { Log.debug("Session restored: \($0)") })
+            .disposed(by: dispose)
     }
 
 }
--- App/SafariExtension/Sources/Classes/SafariExtensionAssembly.swift
+++ App/SafariExtension/Sources/Classes/SafariExtensionAssembly.swift
@@ -6,6 +6,9 @@ import Foundation
 
 import Swinject
 
+import RichHarvest_Core_Core
+import RichHarvest_Domain_Auth_Api
+
 import RichHarvest_Feature_Timer
 import RichHarvest_Feature_Auth
 
@@ -33,10 +36,19 @@ public class SafariExtensionAssembly: Assembly {
 
     public func assemble(container: Container) {
 
+        container.register(SafariExtensionRootViewModel.self) { (r: Resolver) in
+            SafariExtensionRootViewModel(
+                authRepository: r.resolve(AuthRepository.self)!,
+                schedulers: r.resolve(Schedulers.self)!
+            )
+        }
+
         container.register(SafariExtensionRootViewController.self) { (r: Resolver) in
 
             let viewController = NSStoryboard.safariExtensionApp.instantiateInitialController()
                 as! SafariExtensionRootViewController
+
+            viewController.inject(viewModel: r.resolve(SafariExtensionRootViewModel.self)!)
 
             let tabs = [
                 NSTabViewItem(viewController: r.resolve(TimerViewController.self)!),
